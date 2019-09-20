75e014afd160119fb3a55d8006c332d93f87ae47
Jose Valecillos <valecillosjg@gmail.com>
2019-09-20 15:59:12 +0200

    Clean up for activate function

--- src/extension.js
+++ src/extension.js
@@ -9,96 +9,20 @@ const fs = require('fs');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
     // Encrypt command
-    let encDisposable = vscode.commands.registerTextEditorCommand('extension.encryptSelection', function (textEditor) {
-
-        let selection = textEditor.selection;
-        let text = textEditor.document.getText(selection);
-
-        gpg.listKeys().then(
-            publicKeys => publicKeys.map(
-                /** @param {gpg.PublicKey} currentValue */
-                currentValue => ({
-                    label: `<${currentValue.email}>`,
-                    description: `(${currentValue.key_id})`,
-                    detail: currentValue.name,
-                    key_id: currentValue.key_id,
-                    email: currentValue.email,
-                })
-            )
-        ).then(
-            options => vscode.window.showQuickPick(options, { placeHolder: "Select recipient" })
-        ).then(
-            /** @param {{email: string}} selected gpg key option */
-            selected => gpg.encrypt(text, selected.email)
-        ).then(encrypted => {
-
-            textEditor.edit(edit => edit.replace(selection, encrypted));
-
-            vscode.window.setStatusBarMessage('GPG Encrypted!', 2000);
-        }).catch(err => console.error(err));
-    });
-
+    let encSelectionDisp = vscode.commands.registerTextEditorCommand('extension.encryptSelection', encryptSelection);
     // Decrypt command
-    let decDisposable = vscode.commands.registerTextEditorCommand('extension.decryptSelection', editor => {
-        let selection = editor.selection;
-        let text = editor.document.getText(selection);
-
-        new Promise(function (resolve, reject) {
-            // Getting passphrases from configuration as an object "email" =>""passphrase"
-            /**
-             * @type {{email: string, description: string, passphrase: string}[]} passphrases
-            */
-            let passphrases = vscode.workspace.getConfiguration('gpg').get('passphrases');
-
-            let options = passphrases.map(currentValue => ({
-                label: `<${currentValue.email}>`,
-                description: currentValue.description ? `(${currentValue.description})` : '',
-                passphrase: currentValue.passphrase,
-            }));
-
-            if (options && options.length) {
-                vscode.window.showQuickPick(options, { placeHolder: "Select stored passphrase" }).then(selected => {
-                    selected ? resolve(selected.passphrase) : reject("no stored passphrase was selected")
-                });
-            } else {
-                reject("there are no stored passphrases");
-            }
-        }).catch(() => {
-            // Default behaviour ask for the passphrase
-            return vscode.window.showInputBox({
-                prompt: 'Provide your passphrase',
-                placeHolder: 'My passphrase',
-                password: true,
-                validateInput: value => (value.length == 0) ? "Passphrase cannot be empty" : null
-            });
-        }).then(
-            passphrase => {
-                if (passphrase === undefined || passphrase.length === 0) {
-                    // Hitting Enter gets to here, which is not expected
-                    vscode.window.setStatusBarMessage('No passphrase provided', 2000);
-                    return Promise.reject("no passphrase provided")
-                }
-                return gpg.decrypt(text, passphrase)
-            }
-        ).then(
-            decrypted => {
-                editor.edit(editBuilder => editBuilder.replace(selection, decrypted));
-                vscode.window.setStatusBarMessage('GPG Decrypted!', 2000);
-            },
-            error => { console.error("unable to decrypt text", error) }
-        );
-    });
-
-    let encryptArmoredCommand = vscode.commands.registerCommand('extension.encryptArmoredFile', encryptArmored);
-    let decryptFileCommand = vscode.commands.registerCommand('extension.decryptFile', decryptFile);
+    let decSelectionDisp = vscode.commands.registerTextEditorCommand('extension.decryptSelection', decryptSelection);
+    // Encrypt armored file command
+    let encArmoredFileDisp = vscode.commands.registerCommand('extension.encryptArmoredFile', encryptArmored);
+    // Decrypt file command
+    let decFileDisp = vscode.commands.registerCommand('extension.decryptFile', decryptFile);
 
     // register commands
-    context.subscriptions.push(encDisposable);
-    context.subscriptions.push(decDisposable);
-    context.subscriptions.push(encryptArmoredCommand);
-    context.subscriptions.push(decryptFileCommand);
+    context.subscriptions.push(encSelectionDisp);
+    context.subscriptions.push(decSelectionDisp);
+    context.subscriptions.push(encArmoredFileDisp);
+    context.subscriptions.push(decFileDisp);
 }
 exports.activate = activate;
 
@@ -106,6 +30,97 @@ exports.activate = activate;
 function deactivate() {
 }
 exports.deactivate = deactivate;
+
+
+/**
+ * Encrypt text selection command
+ *
+ * @param {vscode.TextEditor} textEditor
+ */
+function encryptSelection(textEditor) {
+
+    let selection = textEditor.selection;
+    let text = textEditor.document.getText(selection);
+
+    gpg.listKeys().then(
+        publicKeys => publicKeys.map(
+            /** @param {gpg.PublicKey} currentValue */
+            currentValue => ({
+                label: `<${currentValue.email}>`,
+                description: `(${currentValue.key_id})`,
+                detail: currentValue.name,
+                key_id: currentValue.key_id,
+                email: currentValue.email,
+            })
+        )
+    ).then(
+        options => vscode.window.showQuickPick(options, { placeHolder: "Select recipient" })
+    ).then(
+        /** @param {{email: string}} selected gpg key option */
+        selected => gpg.encrypt(text, selected.email)
+    ).then(encrypted => {
+
+        textEditor.edit(edit => edit.replace(selection, encrypted));
+
+        vscode.window.setStatusBarMessage('GPG Encrypted!', 2000);
+    }).catch(err => console.error(err));
+}
+
+/**
+ * Decrypt text selection command
+ *
+ * @param {vscode.TextEditor} textEditor
+ */
+function decryptSelection(textEditor)
+{
+    let selection = textEditor.selection;
+    let text = textEditor.document.getText(selection);
+
+    new Promise(function (resolve, reject) {
+        // Getting passphrases from configuration as an object "email" =>""passphrase"
+        /**
+         * @type {{email: string, description: string, passphrase: string}[]} passphrases
+        */
+        let passphrases = vscode.workspace.getConfiguration('gpg').get('passphrases');
+
+        let options = passphrases.map(currentValue => ({
+            label: `<${currentValue.email}>`,
+            description: currentValue.description ? `(${currentValue.description})` : '',
+            passphrase: currentValue.passphrase,
+        }));
+
+        if (options && options.length) {
+            vscode.window.showQuickPick(options, { placeHolder: "Select stored passphrase" }).then(selected => {
+                selected ? resolve(selected.passphrase) : reject("no stored passphrase was selected")
+            });
+        } else {
+            reject("there are no stored passphrases");
+        }
+    }).catch(() => {
+        // Default behaviour ask for the passphrase
+        return vscode.window.showInputBox({
+            prompt: 'Provide your passphrase',
+            placeHolder: 'My passphrase',
+            password: true,
+            validateInput: value => (value.length == 0) ? "Passphrase cannot be empty" : null
+        });
+    }).then(
+        passphrase => {
+            if (passphrase === undefined || passphrase.length === 0) {
+                // Hitting Enter gets to here, which is not expected
+                vscode.window.setStatusBarMessage('No passphrase provided', 2000);
+                return Promise.reject("no passphrase provided")
+            }
+            return gpg.decrypt(text, passphrase)
+        }
+    ).then(
+        decrypted => {
+            textEditor.edit(editBuilder => editBuilder.replace(selection, decrypted));
+            vscode.window.setStatusBarMessage('GPG Decrypted!', 2000);
+        },
+        error => { console.error("unable to decrypt text", error) }
+    );
+}
 
 /**
  * Encrypt armored file command
